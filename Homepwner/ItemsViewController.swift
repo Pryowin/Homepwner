2188a45693d3062024f44111ad618993562386ff
David Burke <david.burke@fundingcircle.com>
2017-07-15 20:27:53 -0700

    Updating details added

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -38,6 +38,11 @@ class ItemsViewController: UITableViewController {
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 65
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
     }
