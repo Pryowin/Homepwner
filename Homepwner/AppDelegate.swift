502a4031ebebc361ceb3f2797eebc7ae9e616097
David Burke <david.burke@fundingcircle.com>
2017-07-15 19:13:02 -0700

    Added Navigation Controller

--- Homepwner/AppDelegate.swift
+++ Homepwner/AppDelegate.swift
@@ -21,8 +21,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let itemStore = ItemStore()
         
         // Access the ItemViewController and set its item store
-        let itemsController = window!.rootViewController as! ItemsViewController
+//        let itemsController = window!.rootViewController as! ItemsViewController
+ 
+        
+        let navController = window!.rootViewController as! UINavigationController
+        let itemsController = navController.topViewController as! ItemsViewController
         itemsController.itemStore = itemStore
+        
         return true
     }
 
